239ad0522ae4b10e26f23c801703f8cf2588af10
Tomas Mengon <123125239+Tommas138@users.noreply.github.com>
2025-09-03 22:32:12 -0300

    Add files via upload

---
+++ CONTROL/TP2/EJ2/EJ3/destinoPOST.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    </head>
+
+    <body>
+
+    <?php
+    if ($_GET) {
+        $nombre = $_GET['nombre_form'];
+        $apellido = $_GET['apellido_form'];
+        $edad= $_GET['edad_form'];
+        $direccion = $_GET['direccion_form'];
+        $cadena = (
+            "
+            Hola yo soy $nombre, $apellido tengo $edad años y vivo en $direccion
+            "
+        );
+        echo "<h3>$cadena</h3>";
+    } else {
+        echo "No se encontraron datos";
+    }
+    ?>
+
+
+    </body>
+
+
+</html>
