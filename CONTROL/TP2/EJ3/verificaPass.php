4c974647ca3a2c21dafb6de18dbdd5e943b41454
Tomas Mengon <tomaso200208@gmail.com>
2025-09-03 21:52:21 -0300

    -

---
+++ CONTROL/TP2/EJ3/verificaPass.php
@@ -0,0 +1,50 @@
+<html>
+
+    <head>
+    </head>
+
+    <body>
+
+    <?php
+
+    $usuario1 = [
+        "usuario" => "Tomas",
+        "clave" => "12345678"
+    ];
+
+    $datosUsuarios = [$usuario1];
+
+    if ($_POST['usuario'] && $_POST['password']) {
+        $user = $_POST['usuario'];
+        $pass = $_POST['password'];
+
+        $cantUsuarios = count($datosUsuarios);
+        $encontrado = false;
+        $cadena = '';
+        $i = 0;
+
+        while ($i < $cantUsuarios && !$encontrado) {
+            $usuarioActual = $datosUsuarios[$i];
+            $userAct = $usuarioActual['usuario'];
+            $passAct = $usuarioActual['clave'];
+
+            if ($user == $userAct && $pass == $passAct) {
+                $encontrado = true;
+                $cadena = "<h2>Bienvenido $userAct</h2>";
+            }
+
+            $i++;
+        }
+
+        if (!$encontrado) {
+            $cadena = "<h2>Usuario o clave incorrectas</h2>";
+        }
+
+        echo $cadena;
+    } else {
+        echo "<h2>Faltan datos</h2>";
+    }
+
+    ?>
+    </body>
+</html>
